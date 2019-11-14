a6d81cdda59436b9a048d21ab810b6156433d71e
Nikola Zelovic <zeln2901@usherbrooke.ca>
2019-11-14 12:37:42 -0500

    Started code for random movement and throwing objects

--- Chaton/src/main.ino
+++ Chaton/src/main.ino
@@ -8,60 +8,88 @@ Date: Derniere date de modification
 
 #include <LibRobus.h>
 
-#define VERT 1
-#define BLEU 2
-#define JAUNE 3
-#define ROUGE 4
+#define PERIMETER 10
 
-#define ROBOT_A
-//#define ROBOT_B
+#define CAPTEUR_PROX_ARRIERE_INDEX 0
+#define CAPTEUR_PROX_AVANT_INDEX 1
+#define IR_TRIGGER 300 // ~ 15 cm
 
-//#define COULEUR VERT
-#define COULEUR ROUGE
-//#define COULEUR JAUNE
-//#define COULEUR ROUGE
+#define SERVO_ARRIERE_INDEX 0
+#define SERVO_AVANT_INDEX 1
 
 const float kp=1E-4;
 const float ki=6E-7;
 
+void forwardPID(float base_speed, float distance, void (*onObstacle)(float,float));
+void tourner(int x);
+void tournerSurPlace(int x);
+void stop();
+
+void throwObject();
+bool detectObstacle();
+int onObstacle();
+void onObstacle_PID(float base_speed, float last_speed);
+
 
 void setup(){
   BoardInit();
+  SERVO_Enable(SERVO_ARRIERE_INDEX);
+  SERVO_Enable(SERVO_AVANT_INDEX);
 }
 
-
-bool stopped = false;
+float speed;
+unsigned started = false;
 void loop() {
- 
+  while(!started && !ROBUS_IsBumper(LEFT)) delay(50);
+  started = true;
 
+  //Random movements
+  /*
+  1: Forward
+  2: Backward
+  3: Turn Left
+  4: Turn Right
+  5: Turn on itself
+  6: Stop for a few seconds
+   */
+  unsigned mov = random(1) + 1;//6 is good value
+  //distance to do  movement. Max is 5 meters/seconds
+  unsigned dist = random(6) * 100;
+  speed = random(1,4) / 10.f;//Speed between 0.1f and 0.4f
+  Serial.print("MOV: ");
+  Serial.println(mov);
 
+  switch(mov){
+    case 1:
+      forwardPID(speed, dist, onObstacle_PID);
+      break;
+    case 2:
+      forwardPID(-speed, dist, onObstacle_PID);
+      break;
+    case 3: 
+      tourner(-dist);
+      break;
+    case 4:
+      tourner(dist);
+      break;
+    case 5:
+      tournerSurPlace(dist);
+      break;
+    case 6:
+      stop();
+      delay(dist*10);
+  }
 
-if(!stopped){
-    SERVO_Enable(0);
+  stop();//Todo remove this after testing !
 
-
-#ifdef ROBOT_A
-  robot_A();
-#elif defined(ROBOT_B)
-  robot_B();
-#endif
-
-  SERVO_Disable(0);
-
-
-stop();
-  stopped = true;
+  delay(100000);
 }
 
-delay(1000);
-   
-
-}
 void tourner (int x)
 {
   int nbtours;
   int roue = x>0?0:1;//>0 à droite, <0 à gauche
-  x*=1995;
+  x*=997;
   x*=x>0?1:-1;
   MOTOR_SetSpeed(roue,0.35);
   MOTOR_SetSpeed(!roue,0);
@@ -73,7 +101,7 @@ void tourner (int x)
   } 
 }  
 
-void forwardPID(float base_speed, float distance)
+void forwardPID(float base_speed, float distance, void (*onObstacle)(float,float))
 {
   //reset the encoders before moving
   ENCODER_Reset(LEFT);
@@ -89,11 +117,11 @@ void forwardPID(float base_speed, float distance)
   MOTOR_SetSpeed(LEFT, base_speed);
   MOTOR_SetSpeed(RIGHT, base_speed);
   unsigned long last_time=millis();
-
+  unsigned long for_delay_millis = millis();
   while (abs(total_encoder) < abs(goal_encoder))
   {
     //Delay to ajust at 10Hz
-    delay(last_time+100-millis());
+    delay(last_time+100-for_delay_millis);
     last_time = millis();
 
     total_encoder += ENCODER_Read(LEFT);
@@ -108,26 +136,15 @@ void forwardPID(float base_speed, float distance)
     MOTOR_SetSpeed(RIGHT, last_speed); 
 
     //Serial.println(total_encoder);
+    
+    for_delay_millis = millis();
+    (*onObstacle)(base_speed, last_speed);
   }
 
   //MOTOR_SetSpeed(LEFT,0);
   //MOTOR_SetSpeed(RIGHT,0);
 }
-void demitour (void)
-{
-  int nbtours = ENCODER_ReadReset(0);
-  ENCODER_Reset(1);
-  MOTOR_SetSpeed(0,0.15);
-  MOTOR_SetSpeed(1,-0.15);
-  while ( nbtours < 3989)
-    {
-      delay(10);
-      nbtours=(abs(ENCODER_Read(0)) + abs(ENCODER_Read(1)))*0.5;
-    }
-   
-  //MOTOR_SetSpeed(0,0);
-  //MOTOR_SetSpeed(1,0);
-} 
+
 
 void stop()
 {
@@ -148,115 +165,56 @@ void tournerSurPlace(int x){
       delay(10);
       nbtours=(abs(ENCODER_Read(0)) + abs(ENCODER_Read(1)))*0.5;
     }
-   
-  //MOTOR_SetSpeed(0,0);
-  //MOTOR_SetSpeed(1,0);
 }
 
-int angA(){
-  switch(COULEUR){
-    case VERT: return 1;
-    case JAUNE: return -3;
-    case ROUGE: return -1;
-    case BLEU: return 3;
+bool detectObstacle(int capteur_id){
+  Serial.println(ROBUS_ReadIR(capteur_id));
+  if(ROBUS_ReadIR(capteur_id) >= IR_TRIGGER){
+    return true;
   }
 
-  return 0;
-}
-int angB(){
-  switch(COULEUR){
-    case VERT: return -3;
-    case JAUNE: return 1;
-    case ROUGE: return 3;
-    case BLEU: return -1;
-  }
-
-  return 0;
+  return false;
 }
 
-#define VITESSE 0.3f
+void throwObject(){
+  SERVO_SetAngle(SERVO_AVANT_INDEX, 180);
+  delay(3000);//Try throwing the object for 3 seconds
+  SERVO_SetAngle(SERVO_AVANT_INDEX, 0);
+}
 
-// Robot A
-void robot_A ()
-{
-  int angle_depart = (COULEUR == ROUGE ? -2 : 2);
-
-  //SE RENDRE JUSQU'A LA BALLE
-  if(COULEUR == ROUGE || COULEUR == VERT){
-    tournerSurPlace(angle_depart);
-    forwardPID(VITESSE, 60);
-    tournerSurPlace(-angle_depart);
-    forwardPID(VITESSE, 100);
-    tournerSurPlace(angA());
-    forwardPID(VITESSE, 30);
-
-  }else{//JAUNE et BLEU
-    forwardPID(VITESSE+0.2, 33);
-    tournerSurPlace(angA());
-    forwardPID(VITESSE, 120);
-  }
-
-  
-  stop();
-  delay(1000);
-  activerPince(false);
-  stop();
-  delay(1000);
-  forwardPID(VITESSE-0.1f, 3);
-  stop();
-  delay(1000);
-  forwardPID(-VITESSE, 30);
-
- if(COULEUR == ROUGE || COULEUR == VERT){
-    tournerSurPlace(COULEUR == ROUGE ? -3 : 3);
-    forwardPID(VITESSE, 63);
-    tournerSurPlace(angle_depart);
-    forwardPID(VITESSE, 20);
-    forwardPID(VITESSE+0.1f, 30);
- }else{
-   demitour();
-  forwardPID(VITESSE, 66);
- }
-
-  stop();
-  delay(1500);
-  activerPince(true);
-  delay(1000);
-  forwardPID(-VITESSE, 35);
-
-  if(COULEUR == JAUNE || COULEUR == BLEU){
-    tournerSurPlace(1);
+/*
+ * Tries to throw down the obstacle if it detects one
+ * @return -1: no obstacles detected,
+ *          false: failed to throw down obstacle,
+ *          true: successfully threw down obstacle
+ */
+int onObstacle(int capteur_id){
+  if(detectObstacle(capteur_id)){
     stop();
+    throwObject();
+    if(detectObstacle(capteur_id)){//Arm was unable to throw the object
+      return false;
+    }
+    return true;
   }
-
-  forwardPID(-VITESSE, 60);
+  return -1;
 }
 
-void robot_B(){
-  activerPince(true);
-  stop();
-  delay(1000);
-
-  forwardPID(VITESSE, 30);
-  
-  activerPince(false);
-  stop();
-  delay(1000);
-
-  tournerSurPlace(angB());
-
-  forwardPID(VITESSE,105);
-  stop();
-  delay(1000);
-
-  activerPince(true);
-  stop();
-  delay(1000);
-  forwardPID(-VITESSE, 30);
-}
-
-void activerPince(bool ouvrir){
-  float ANGLE = ouvrir ? 45 : 143;
-
-  SERVO_SetAngle(0, ANGLE);
+void onObstacle_PID(float base_speed, float last_speed){
+  int onObstacleResult = onObstacle(base_speed > 0 ? CAPTEUR_PROX_AVANT_INDEX : CAPTEUR_PROX_ARRIERE_INDEX);//One Sensor in front and the other in the back. Select which one to check depending on the movement of the robot (goint forwards or backwards)
+  if(onObstacleResult != -1){//Check if the robot stopped because of an obstacle
+    if(onObstacleResult == true){
+      //Successfully threw down obstacle
+      //Reset motors to good speed
+      MOTOR_SetSpeed(LEFT, base_speed);
+      MOTOR_SetSpeed(RIGHT, last_speed); 
+    }
+    else{//Failed to throw down object. Go in the opposite direction
+      MOTOR_SetSpeed(LEFT, -base_speed);
+      MOTOR_SetSpeed(RIGHT, -base_speed);
+      delay(2000);
+      stop();
+      return; //Exit the function and do another movement
+    }
+  }
 }
